bcee36982af1dafec475f5ac0b1c5104fa967c85
shubhangi-modi <shubhangi.modi@globant.com>
2024-11-27 15:29:01 +0530

    first commit project scaffold

---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import MovieDetails from "./MovieDetails";
+import MoviesList from "./MoviesList";
+
+const Body = () => {
+    return (<div>
+        <MoviesList/>
+        <MovieDetails/>
+    </div>)
+}
+
+export default Body;
